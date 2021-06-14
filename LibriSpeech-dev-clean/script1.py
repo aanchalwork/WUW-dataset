5c837aa773f181d5c048df9a5cca82c1e3f75525
Kiriti <kiriti56@outlook.com>
2021-06-14 12:25:30 +0530

    Added Librispeech-dev-clean data set

---
+++ LibriSpeech-dev-clean/script1.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:79f042121bbe9826145f5e3d44acd2dc26aa300868101d999ef154582bfd8aaf
+size 1150
---
+++ LibriSpeech-dev-clean/textgrid2csv.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:568f2784b5135a9ad99323cbc0fac51f4677a6865415e5848b24209cf1b980a7
+size 2156
