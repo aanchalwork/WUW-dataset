fef7086e1cf24773f726b0a853c24521e3114fe6
Kiriti <kiriti56@outlook.com>
2021-06-14 13:45:12 +0530

    Added Librispeech-dev-other data set

---
+++ LibriSpeech-dev-other/script1.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b610b031d000f96ba24248b87635cafc28696665dcf19ec8cd5dad7c53edd417
+size 1189
---
+++ LibriSpeech-dev-other/script3.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1b7197fab5f5813c70397cf5f958c4fc9304d84d9d346167c341f3c36e5984ee
+size 1384
